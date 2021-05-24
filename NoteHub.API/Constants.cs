59cf326d2d5c9ae7c1e2db8983a4cf1bdef6d68d
Galip Yıldız <galipyildiz9606@outlook.com>
2021-05-24 10:32:02 +0300

    Add project files.

---
+++ NoteHub.API/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteHub.API
+{
+    public static class Constants
+    {
+        public const string DEFAULT_PASSWORD = "Ankara1.";
+        public const string DEFAULT_EMAIL = "admin@example.com";
+        public static class Roles
+        {
+            public const string ADMIN = "Admin";
+        }
+    }
+}
