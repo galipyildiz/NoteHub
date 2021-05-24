12ae8ee7b2ae39820d852985ecae8abea6d864a4
Galip Yıldız <galipyildiz9606@outlook.com>
2021-05-24 12:18:05 +0300

    jwt authantication

--- NoteHub.API/Constants.cs
+++ NoteHub.API/Constants.cs
@@ -9,6 +9,7 @@ namespace NoteHub.API
     {
         public const string DEFAULT_PASSWORD = "Ankara1.";
         public const string DEFAULT_EMAIL = "admin@example.com";
+        public const string AUTH_SIGNING_KEY = "Y``SFASf____$aszxc13123-123*/,123zxZ";//ne istersek girebiliriz. İmzamız token için.
         public static class Roles
         {
             public const string ADMIN = "Admin";
--- NoteHub.API/Startup.cs
+++ NoteHub.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,11 +9,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NoteHub.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace NoteHub.API
@@ -34,6 +37,22 @@ namespace NoteHub.API
                 .AddEntityFrameworkStores<NoteHubDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Constants.AUTH_SIGNING_KEY))
+                    };
+                });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -54,7 +73,7 @@ namespace NoteHub.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();//önemli bunuda ekledik. ayarlamalarýný yaptýðýmýz authentication kullan.
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
